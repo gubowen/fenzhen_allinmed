8c6ff38dc71f902aeae157bc27f2ea9ec4872541
谷博文 <d_gubowen@163.com>
2017-11-10 18:19:05 +0800

    1.患者列表 用户接诊选择状态。

---
+++ src/components/imParts/updateTips.vue
@@ -0,0 +1,67 @@
+<template>
+    <article>
+        <figure class="messageList-item-content">
+            <!--患者头像-->
+            <figure class="messageList-item-img">
+                <img :src="$store.state.currentItem.logoUrl" alt="">
+            </figure>
+            <figcaption class="messageList-item-text">
+                <span>{{content}}点击至</span>
+                <a href='#' class='link' @click='goToCheck'>"专科检查"</a>
+                <span>查看。</span>
+                <span v-if="showType==='videoTriage'">若视频上传中，请稍候再次点击查看。</span>
+            </figcaption>
+        </figure>
+    </article>
+</template>
+<script type="text/ecmascript-6">
+    /**
+     * @Desc：
+     * @Usage:
+     * @Notify：
+     * @Depend：
+     *
+     * Created by Qiangkailiang on 17/11/1.
+     */
+    export default{
+        data(){
+            return {
+                content: ""
+            }
+        },
+        mounted(){
+            this.contentSwitch(this.showType);
+        },
+        methods: {
+            contentSwitch(type){
+                let result = "";
+                switch (type) {
+                    case "videoTriage":
+                        result = "患者已上传视诊资料，";
+                        break;
+                    case "imageTriage":
+                    case "checkSuggessSendTips":
+                        result = "患者已上传检查资料，";
+                        break;
+                }
+                this.content=result;
+            },
+            goToCheck(){
+                this.$router.push({
+                    path: "/home/majorCheck/",
+                    params: {
+                        num: JSON.stringify(this.$store.state.currentItem)
+                    }
+                })
+            }
+        },
+        props: {
+            showType: {
+                type: String
+            }
+        }
+    }
+</script>
+<style lang="scss" rel="stylesheet/scss">
+
+</style>
