223684fea956869121e5d57cdaab55a62e275749
wangjingrong <wangjingrong@allinmd.cn>
2017-11-15 13:38:40 +0800

    问题：推荐医生分页还原 reviewer:谷博文

--- src/components/suggestParts/diagnosiSecond.vue
+++ src/components/suggestParts/diagnosiSecond.vue
@@ -384,7 +384,7 @@
                     allDocList: [],
                     allDocState: false,
                     totalCount: 0,
-                    pageNum: 10,
+                    pageNum: 20,
                     pageIndex: 1,
                     pageResult: 0,
                     pageArr:[],
